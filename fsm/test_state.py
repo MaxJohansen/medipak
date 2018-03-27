c9129d711886c22510f03c50c4ec91b6cfa74288
Mads Johansen <johansen.max@gmail.com>
2018-03-27 22:09:32 +0200

    Add rudimentary test for FSM

--- fsm/test_state.py
+++ fsm/test_state.py
@@ -4,7 +4,17 @@ from dummy import DummySensor
 
 
 class TestState(unittest.TestCase):
-    def test_basics(self):
-        fsm = State({'temp': DummySensor([1, 2, 3])})
-        fsm.read_sensors()
+    def test_reads_from_sensor(self):
+        temp_sensor = DummySensor([1, 2, 3])
+        fsm = State({'temp': temp_sensor})
+        fsm.run_one_cycle()
+        self.assertEqual(temp_sensor.num_reads, 1)
         self.assertEqual(fsm.readings, [{'temp': 1}])
+
+    def test_reads_from_all_sensors(self):
+        temp_sensor = DummySensor([])
+        hr_sensor = DummySensor([])
+        fsm = State({'temp': temp_sensor, 'heartrate': hr_sensor})
+        fsm.run_one_cycle()
+        self.assertEqual(temp_sensor.num_reads, 1)
+        self.assertEqual(hr_sensor.num_reads, 1)
