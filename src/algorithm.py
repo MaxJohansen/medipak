2d7ae03a5de004e128a68df3f236073f7ab7b757
Raymon Hansen <aussie_raymon@hotmail.com>
2018-04-17 15:32:20 +0200

    Fuck this algorithm

--- src/algorithm.py
+++ src/algorithm.py
@@ -1,3 +1,22 @@
 """Algorithm module to calculate heartrate from 
 pulse sensor - RD117."""
 
+def max_heart_rate_and_oxygen_sat(ir_buffer, red_buffer):
+    """Calculate heart rate and Sp02 level. Keeping close to the original
+    horror of 'algorithm.cpp' until we know what it does...
+    ir_buffer - list()
+    red_buffer - list()
+    return 
+    """
+    # calculate DC mean and subtract DC from ir
+    ir_mean = sum(ir_buffer) / len(ir_buffer)
+
+    # remove DC and invert signal so that we can
+    # use peak detector as valley detector
+    an_x = [ir_mean - num for num in ir_buffer]
+
+    moving = []
+    for nums in zip(an_x, an_x[1:], an_x[2:], an_x[3:]):
+        moving.append(sum(nums) / 4)
+
+    # Continue on line 117
