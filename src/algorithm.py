516814616d25aab2f8dfdd4e41dc4031460cae0c
Raymon Hansen <aussie_raymon@hotmail.com>
2018-04-18 11:37:40 +0200

    More work on algorithm.

--- src/algorithm.py
+++ src/algorithm.py
@@ -15,8 +15,18 @@ def max_heart_rate_and_oxygen_sat(ir_buffer, red_buffer):
     # use peak detector as valley detector
     an_x = [ir_mean - num for num in ir_buffer]
 
+    # Calculate 4 point moving average
+    # OSB! This ignores the last three values!?
     moving = []
     for nums in zip(an_x, an_x[1:], an_x[2:], an_x[3:]):
         moving.append(sum(nums) / 4)
 
-    # Continue on line 117
+    # Calculate threshold based on the moving average
+    n_th1 = sum(moving) / len(ir_buffer)
+    
+    # Clamp threshold between 30 and 60 because...?!
+    n_th1 = max(min(n_th1, 60), 30)
+
+    # Use peak detector as valley-detector
+    ir_valley_locs = [0] * 15
+    maxim_find_peaks()
